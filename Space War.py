c47fc4ce1285b5b2c04dacca7ed826486738a902
JA1221 <cat605101@yahoo.com.tw>
2019-11-06 18:50:24 +0800

    replace tab - > space*4

--- Space War.py
+++ Space War.py
@@ -31,93 +31,196 @@ pygame.mixer.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("太空戰爭")
-clock = pygame.time.Clock()	 ## For syncing the FPS
+clock = pygame.time.Clock()     ## For syncing the FPS
 ###############################
 
 #遊戲主畫面
 def main_menu():
-	global screen
+    global screen
 
-	menu_song = pygame.mixer.music.load(path.join(sound_folder, "menu.mp3"))
-	pygame.mixer.music.play(-1)
+    menu_song = pygame.mixer.music.load(path.join(sound_folder, "menu.mp3"))
+    pygame.mixer.music.play(-1)
 
-	title = pygame.image.load(path.join(img_folder, "main.png")).convert()
-	title = pygame.transform.scale(title, (WIDTH, HEIGHT), screen)#縮放
+    title = pygame.image.load(path.join(img_folder, "main.png")).convert()
+    title = pygame.transform.scale(title, (WIDTH, HEIGHT), screen)#縮放
 
-	screen.blit(title, (0,0))
+    screen.blit(title, (0,0))
 
-	draw_text(screen, "按下 [ENTER] 開始遊戲", 30, WIDTH/2, HEIGHT/2)
-	draw_text(screen, "or [Q] 離開", 30, WIDTH/2, (HEIGHT/2)+40)
-	pygame.display.update()
-	while True:
-		event = pygame.event.poll() # 取一個事件
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_RETURN:#進入遊戲
-				break
-			elif event.key == pygame.K_q:#離開
-				pygame.quit()
-				quit()
-		elif event.type == pygame.QUIT:
-				pygame.quit()
-				quit() 
-			
+    draw_text(screen, "按下 [ENTER] 開始遊戲", 30, WIDTH/2, HEIGHT/2)
+    draw_text(screen, "or [Q] 離開", 30, WIDTH/2, (HEIGHT/2)+40)
+    pygame.display.update()
+    while True:
+        event = pygame.event.poll() # 取一個事件
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:#進入遊戲
+                break
+            elif event.key == pygame.K_q:#離開
+                pygame.quit()
+                quit()
+        elif event.type == pygame.QUIT:
+                pygame.quit()
+                quit() 
+            
 
-	#pygame.mixer.music.stop()
-	ready = pygame.mixer.Sound(path.join(sound_folder,'getready.ogg'))
-	ready.play()
-	screen.fill(BLACK)
-	draw_text(screen, "倒數三秒鐘!", 40, WIDTH/2, HEIGHT/2)
-	pygame.display.update()
+    #pygame.mixer.music.stop()
+    ready = pygame.mixer.Sound(path.join(sound_folder,'getready.ogg'))
+    ready.play()
+    screen.fill(BLACK)
+    draw_text(screen, "倒數三秒鐘!", 40, WIDTH/2, HEIGHT/2)
+    pygame.display.update()
+#********************* 物件 ***********************
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        ## scale the player img down
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.radius = 20
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0 
+        self.shield = 100
+        self.shoot_delay = 250
+        self.last_shot = pygame.time.get_ticks()
+        self.lives = 3
+        self.hidden = False
+        self.hide_timer = pygame.time.get_ticks()
+        self.power = 1
+        self.power_timer = pygame.time.get_ticks()
+
+    def update(self):
+        ## time out for powerups
+        if self.power >=2 and pygame.time.get_ticks() - self.power_time > POWERUP_TIME:
+            self.power -= 1
+            self.power_time = pygame.time.get_ticks()
+
+        ## unhide 
+        if self.hidden and pygame.time.get_ticks() - self.hide_timer > 1000:
+            self.hidden = False
+            self.rect.centerx = WIDTH / 2
+            self.rect.bottom = HEIGHT - 30
+
+        self.speedx = 0     ## makes the player static in the screen by default. 
+        # then we have to check whether there is an event hanlding being done for the arrow keys being 
+        ## pressed 
+
+        ## will give back a list of the keys which happen to be pressed down at that moment
+        keystate = pygame.key.get_pressed()     
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 5
+        if keystate[pygame.K_UP]:
+            self.speedy = -5
+        if keystate[pygame.K_DOWN]:
+            self.speedy = -5
+
+
+        #Fire weapons by holding spacebar
+        if keystate[pygame.K_SPACE]:
+            self.shoot()
+
+        ## check for the borders at the left and right
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+        self.rect.x += self.speedx
+
+    def shoot(self):
+        ## to tell the bullet where to spawn
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > self.shoot_delay:
+            self.last_shot = now
+            if self.power == 1:
+                bullet = Bullet(self.rect.centerx, self.rect.top)
+                all_sprites.add(bullet)
+                bullets.add(bullet)
+                shooting_sound.play()
+            if self.power == 2:
+                bullet1 = Bullet(self.rect.left, self.rect.centery)
+                bullet2 = Bullet(self.rect.right, self.rect.centery)
+                all_sprites.add(bullet1)
+                all_sprites.add(bullet2)
+                bullets.add(bullet1)
+                bullets.add(bullet2)
+                shooting_sound.play()
+
+            """ MOAR POWAH """
+            if self.power >= 3:
+                bullet1 = Bullet(self.rect.left, self.rect.centery)
+                bullet2 = Bullet(self.rect.right, self.rect.centery)
+                missile1 = Missile(self.rect.centerx, self.rect.top) # Missile shoots from center of ship
+                all_sprites.add(bullet1)
+                all_sprites.add(bullet2)
+                all_sprites.add(missile1)
+                bullets.add(bullet1)
+                bullets.add(bullet2)
+                bullets.add(missile1)
+                shooting_sound.play()
+                missile_sound.play()
+
+    def powerup(self):
+        self.power += 1
+        self.power_time = pygame.time.get_ticks()
+
+    def hide(self):
+        self.hidden = True
+        self.hide_timer = pygame.time.get_ticks()
+        self.rect.center = (WIDTH / 2, HEIGHT + 200)
+#**************************************************
 
 #顯示 文字
 def draw_text(surf, text, size, x, y):
-	font = pygame.font.Font("msjh.ttf", size)
-	text_surface = font.render(text, False, WHITE)
-	text_rect = text_surface.get_rect()
-	text_rect.midtop = (x, y)
-	surf.blit(text_surface, text_rect)
+    font = pygame.font.Font("msjh.ttf", size)
+    text_surface = font.render(text, False, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
 
 #顯示 護甲
 def draw_shield_bar(surf, x, y, pct):
-	pct = max(pct, 0) 
+    pct = max(pct, 0) 
 
-	shield_NUM = (pct / 100) * BAR_LENGTH
-	total_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
-	shield_rect = pygame.Rect(x, y, shield_NUM, BAR_HEIGHT)
-	
-	pygame.draw.rect(surf, BLUE, shield_rect)
-	pygame.draw.rect(surf, WHITE, total_rect, 2)
+    shield_NUM = (pct / 100) * BAR_LENGTH
+    total_rect = pygame.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    shield_rect = pygame.Rect(x, y, shield_NUM, BAR_HEIGHT)
+    
+    pygame.draw.rect(surf, BLUE, shield_rect)
+    pygame.draw.rect(surf, WHITE, total_rect, 2)
 
 #顯示 
 def draw_lives(surf, x, y, lives, img):
-	for i in range(lives):
-		img_rect= img.get_rect()
-		img_rect.x = x + 30 * i
-		img_rect.y = y
-		surf.blit(img, img_rect)
+    for i in range(lives):
+        img_rect= img.get_rect()
+        img_rect.x = x + 30 * i
+        img_rect.y = y
+        surf.blit(img, img_rect)
 
 #############################
 ## Game loop
 running = True
 menu_display = True
 while running:
-	if menu_display:
-		main_menu()
-		pygame.time.wait(1000)
+    if menu_display:
+        main_menu()
+        pygame.time.wait(1000)
 
-	clock.tick(FPS)
-	for event in pygame.event.get():
-		#關閉視窗
-		if event.type == pygame.QUIT:
-			running = False
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        #關閉視窗
+        if event.type == pygame.QUIT:
+            running = False
 
-		#ESC離開遊戲
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
-				running = False
-	draw_shield_bar(screen, 10, 10, 50)#test
-	pygame.display.flip()	
-	pygame.time.wait(1000)
+        #ESC離開遊戲
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+    draw_shield_bar(screen, 10, 10, 50)#test
+    pygame.display.flip()    
+    pygame.time.wait(1000)
 
 
 pygame.quit()
