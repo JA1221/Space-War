cbe0a3168d3cd4df24d4e009404216005743c360
JA1221 <cat605101@yahoo.com.tw>
2019-11-07 07:17:19 +0800

    add explosion & meteors<-> bullets expl

--- Space War.py
+++ Space War.py
@@ -30,7 +30,7 @@ pygame.init()
 pygame.mixer.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("太空戰爭")
+pygame.display.set_caption('太空戰爭')
 clock = pygame.time.Clock()     ## For syncing the FPS
 ###############################
 #遊戲主畫面
@@ -38,7 +38,7 @@ def main_menu():
     global screen
 
     # 載入主畫面音樂
-    menu_song = pygame.mixer.music.load(path.join(sound_folder, "menu.mp3"))
+    menu_song = pygame.mixer.music.load(path.join(sound_folder, 'menu.mp3'))
     # pygame.mixer.music.play(-1)
 
     # 背景圖片
@@ -228,7 +228,7 @@ class Meteor(pygame.sprite.Sprite):
         if (self.rect.top > HEIGHT) or (self.rect.left > WIDTH) or (self.rect.right < 0):
             self.rect.x = random.randrange(0, WIDTH - self.rect.width)
             self.rect.y = random.randrange(-100, -40)
-            self.speedy = random.randrange(1, 8)        ## for randomizing the speed of the Mob
+            self.speedy = random.randrange(1, 8)
 # 子彈
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -261,6 +261,31 @@ class Missile(pygame.sprite.Sprite):
         self.rect.y += self.speedy
         if self.rect.bottom < 0:
             self.kill()
+
+# 爆炸 (lg,sm,player)
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, center, size):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = size
+        self.image = explosion_anim[self.size][0]
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+        self.frame = 0 
+        self.last_update = pygame.time.get_ticks()
+        self.frame_delay = 75 # 每張圖片間距時間
+
+    def update(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > self.frame_delay:
+            self.last_update = now
+            self.frame += 1
+            if self.frame == len(explosion_anim[self.size]):
+                self.kill()
+            else:
+                center = self.rect.center
+                self.image = explosion_anim[self.size][self.frame]
+                self.rect = self.image.get_rect()
+                self.rect.center = center
 #**************************************************
 
 #顯示 文字
@@ -292,8 +317,8 @@ def draw_lives(surf, x, y, lives, img):
 
 def newMeteor():
     newMeteor = Meteor()
-    # all_sprites.add(newMeteor)
-    Meteors.add(newMeteor)
+    all_sprites.add(newMeteor)
+    meteors.add(newMeteor)
 ###################################################
 # 載入圖片
 
@@ -320,37 +345,67 @@ meteor_list = [
 for image in meteor_list:
     meteor_images.append(pygame.image.load(path.join(img_folder, image)).convert_alpha())
 
-#飛彈
+# 飛彈
 bullet_img = pygame.image.load(path.join(img_folder, 'laserRed16.png')).convert_alpha()
 missile_img = pygame.image.load(path.join(img_folder, 'missile.png')).convert_alpha()
+
+# 爆炸
+explosion_anim = {}
+explosion_anim['lg'] = []
+explosion_anim['sm'] = []
+explosion_anim['player'] = []
+
+for i in range(9):
+    filename = 'regularExplosion0{}.png'.format(i)
+    img = pygame.image.load(path.join(img_folder, filename)).convert_alpha()
+
+    # 爆炸 two size(隕石)
+    img_lg = pygame.transform.scale(img, (75, 75))
+    explosion_anim['lg'].append(img_lg)
+    img_sm = pygame.transform.scale(img, (32, 32))
+    explosion_anim['sm'].append(img_sm)
+
+    # 玩家爆炸
+    filename = 'sonicExplosion0{}.png'.format(i)
+    img = pygame.image.load(path.join(img_folder, filename)).convert_alpha()
+    explosion_anim['player'].append(img)
 ###################################################
 #載入音樂
 
 shooting_sound = pygame.mixer.Sound(path.join(sound_folder, 'pew.wav'))
 missile_sound = pygame.mixer.Sound(path.join(sound_folder, 'rocket.ogg'))
 
+expl_sounds = []
+for sound in ['expl3.wav', 'expl6.wav']:
+    expl_sounds.append(pygame.mixer.Sound(path.join(sound_folder, sound)))
+
+pygame.mixer.music.set_volume(0.2)
 ###################################################
 ## Game loop
 running = True
 menu_display = True
 while running:
-    # 遊戲主畫面
+    # 1.遊戲主畫面
     if menu_display:
         main_menu()
         pygame.time.wait(1000)
         menu_display = False
 
-        #create Group
+        # create Group
         bullets = pygame.sprite.Group()
         all_sprites = pygame.sprite.Group()
-        Meteors = pygame.sprite.Group()
-        # 建立Meteors
+        meteors = pygame.sprite.Group()
+        # 建立meteors
         for i in range(8):
             newMeteor()
         # 建立玩家
         player = Player()
         all_sprites.add(player)
 
+        # 分數
+        score = 0
+
+    # 2.幀數控制 輸入偵測
     clock.tick(FPS)
     for event in pygame.event.get():
         #關閉視窗
@@ -361,19 +416,35 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+
+    # 3.精靈更新
+    all_sprites.update()
+
+    # 4.偵測 飛彈&隕石 碰撞
+    hits = pygame.sprite.groupcollide(meteors, bullets, True, False) # (隕石, 子彈, 隕石刪除TRUE, 子彈刪除TRUE)
+    
+    for hit in hits:
+        print(hit.radius)
+        score += 60 - hit.radius
+        random.choice(expl_sounds).play()
+
+        expl = Explosion(hit.rect.center, 'lg')
+        all_sprites.add(expl)
+
+        newMeteor()
+
+   
+
+    
     
     screen.fill(BLACK)
     screen.blit(background, background_rect)
 
-    draw_shield_bar(screen, 10, 10, 50)#test
-    all_sprites.update()
+    draw_shield_bar(screen, 10, 10, player.shield)#test
     all_sprites.draw(screen)
 
-    Meteors.update()
-    Meteors.draw(screen)
-
     pygame.display.flip() 
-    pygame.display.set_caption("Space Shooter " + str(int(clock.get_fps())) + " fps")    
+    pygame.display.set_caption('太空戰爭 ' + str(int(clock.get_fps())) + " fps")    
 
 
 pygame.quit()
